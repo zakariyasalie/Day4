3eb3e3c687fa23535418678dff794ee8fa155072
zakariyasalie <zakariya.salie07@gmail.com>
2024-05-17 14:18:48 +0200

    update

---
+++ script/code.js
@@ -0,0 +1,21 @@
+// First object
+var person1 = {
+    name: "John Doe",
+    age: 30,
+    gender: "male",
+    profession: "engineer"
+  };
+  console.log(person1);
+  
+  // Second object with different values but same properties
+  var person2 = {
+    name: "Jane Smith",
+    age: 28,
+    gender: "female",
+    profession: "doctor"
+  };
+  console.log(person2);
+  
+  // Creating an array and adding both objects to it
+  var personArray = [person1, person2];
+  console.log(personArray);
